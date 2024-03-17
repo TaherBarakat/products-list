8a803fcbfbb047a82da6fb872f9e38a5d8f896d7
Taher Barakat <taher.barakat12223@gmail.com>
2024-03-17 14:47:28 +0300

    add the extra buttons to the ui

--- src/components/item/items.js
+++ src/components/item/items.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React from "react";
 
 const Items = (props) => {
-    const {items, del} = props;
-    let length = items.length
-    const ListItem = length ? (
-        items.map(item => {
-            return(
-                <div key={item.id} className="item">
-                    <p>{item.product}</p>
-                    <p>{item.price}</p>
-                    <p className="delete" onClick={() => del(item.id)}>&times;</p>
-                </div>
-            )
-        })
-    ) : (
-        <div className="text">There are no items, Try to add some.</div>
-    )
-    return (
-        <div>
-            <div className="header item">
-                <p>Product</p>
-                <p>Price</p>
-                <p>Edit</p>
-            </div>
-            {ListItem}
-        </div>
-    )
-}
+     const { items, del, inc, dec } = props;
+     //  console.log(1);
+     let length = items.length;
+     const ListItem = length ? (
+          items.map((item) => {
+               return (
+                    <div key={item.id} className="item">
+                         <p>{item.product}</p>
+                         <p>{item.price}</p>
+                         <p>{item.quantity}</p>
+                         <p
+                              className="edit delete"
+                              onClick={() => del(item.id)}
+                         >
+                              &times;
+                         </p>
+                         <p
+                              className="edit increase"
+                              onClick={() => inc(item.id)}
+                         >
+                              &#43;
+                         </p>
+                         <p
+                              className="edit decrease "
+                              onClick={() => dec(item.id)}
+                         >
+                              &#45;
+                         </p>
+                    </div>
+               );
+          })
+     ) : (
+          <div className="text">There are no items, Try to add some.</div>
+     );
+     return (
+          <div>
+               <div className="header item">
+                    <p>Product</p>
+                    <p>Price</p>
+                    <p>quantity</p>
+                    <p className="edit ">Delete</p>
+                    <p className="edit ">increase</p>
+                    <p className="edit ">decrease</p>
+               </div>
+               {ListItem}
+          </div>
+     );
+};
 
-export default Items
+export default Items;
